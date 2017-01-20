2a80b71d7a5e9704b7a178de9798f951204cb242
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-01-20 20:41:28 +0200

    * finalized api

--- classes/NoSQLConnectionFactory.php
+++ classes/NoSQLConnectionFactory.php
@@ -1,57 +1,82 @@
 <?php
 /**
- * Implements a database connection manager on top of NoSQLConnection.
+ * Implements a singleton factory for multiple NoSQL servers connection.
  */
 final class NoSQLConnectionFactory {
 	/**
-	 * Stores open connections in session.
+	 * Stores open connections.
 	 * 
 	 * @var array
 	 */
-	private static $connections;
+	private static $instances;
 	
 	/**
-	 * Stores registered data sources in session.
+	 * Stores registered data sources.
 	 * @var array
 	 */
 	private static $dataSources;
 	
+    /**
+     * @var NoSQLDBServer
+     */
+    private $database_connection = null;
+	
 	/**
-	 * Registers connection information (data source) for later use, based on server name.
+	 * Registers a data source object encapsulatings connection info based on unique server identifier.
 	 * 
-	 * @param string $strServerName
-	 * @param SQLDataSource $objDataSource
+	 * @param string $strServerName Unique identifier of server you will be connecting to.
+	 * @param NoSQLDataSource $objDataSource
 	 */
-	public static function register($strServerName, $objDataSource){
+	public static function setDataSource($strServerName, NoSQLDataSource $objDataSource){
 		self::$dataSources[$strServerName] = $objDataSource;
 	}
 	
 	/**
-	 * Opens connection to database server (if not already open) according to saved information on the basis of server name and returns SQLConnection object. 
+	 * Opens connection to database server (if not already open) according to NoSQLDataSource and 
+	 * returns an object of that connection to delegate operations to.
 	 * 
-	 * @param string $strServerName
-	 * @throws SQLException
-	 * @return SQLConnection
+	 * @param string $strServerName Unique identifier of server you will be connecting to.
+	 * @throws NoSQLConnectionException
+	 * @return NoSQLDBOperations
 	 */
-	public static function connect($strServerName){
-		if(!isset(self::$connections[$strServerName])) {
-			if(!isset(self::$dataSources[$strServerName])) throw new SQLException("Datasource not set for: ".$strServerName);
-			$objConnection = new SQLConnection();
-			$objConnection->connect(self::$dataSources[$strServerName]);
-			self::$connections[$strServerName] = $objConnection;
-		}
-		return self::$connections[$strServerName];
+	public static function getInstance($strServerName){
+        if(isset(self::$instances[$strServerName])) {
+            return self::$instances[$strServerName];
+        }
+        self::$instances[$strServerName] = new NoSQLConnectionFactory($strServerName);
+		return self::$instances[$strServerName];
+	}
+
+
+	/**
+	 * Connects to database automatically.
+	 *
+	 * @throws NoSQLConnectionException
+	 */
+	private function __construct($strServerName) {
+		if(!isset(self::$dataSources[$strServerName])) throw new NoSQLConnectionException("Datasource not set for: ".$strServerName);
+		$className = str_replace("DataSource","Driver",get_class(self::$dataSources[$strServerName]));
+		if(!class_exists($className)) throw new NoSQLConnectionException("Class not found: ".$className);
+		$this->database_connection = new $className();
+		$this->database_connection->connect(self::$dataSources[$strServerName]);
 	}
 	
 	/**
-	 * Closes connection to database server according to server name
-	 * 
-	 * @param string $strServerName
+	 * Internal utility to get connection.
+	 *
+	 * @return NoSQLDBOperations
 	 */
-	public static function disconnect($strServerName) {
-		if(isset(self::$connections[$strServerName])) {
-			self::$connections[$strServerName]->disconnect();
-			unset(self::$connections[$strServerName]);
-		}
+	private function getConnection() {
+		return $this->database_connection;
 	}
+	
+	/**
+	 * Disconnects from database server automatically.
+	 */
+	public function __destruct() {
+		try {
+			$this->database_connection->disconnect();
+		} catch(Exception $e) {}
+	}
+	
 }
