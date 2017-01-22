a4b53b6f4303e59582353fff5679283422b2673c
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2017-01-22 12:56:10 +0200

    * renamed folders

--- loader.php
+++ loader.php
@@ -1,2 +1,8 @@
 <?php
-require_once("classes/NoSQLConnection.php");
+require_once("src/NoSQLDataSource.php");
+require_once("src/NoSQLConnection.php");
+require_once("src/NoSQLServer.php");
+require_once("src/NoSQLConnectionFactory.php");
+require_once("src/NoSQLConnectionSingleton.php");
+require_once("src/exceptions/NoSQLConnectionException.php");
+require_once("src/exceptions/NoSQLStatementException.php");
---
+++ src/exceptions/NoSQLStatementException.php
@@ -0,0 +1,2 @@
+<?php
+class NoSQLStatementException extends Exception {}
