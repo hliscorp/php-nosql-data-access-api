18d85e5861c737ad2863c5946fb0a7127a54e890
Lucian Popescu <lucian.gabriel.popescu@gmail.com>
2017-10-31 11:00:14 +0200

    * moved operations to supported versions

--- src/CouchbaseDataSource.php
+++ src/CouchbaseDataSource.php
@@ -23,11 +23,51 @@ class BucketInformation {
 /**
  * Encapsulates couchbase server connection & bucket data.
 */
-class CouchbaseDataSource extends NoSQLDataSource {
+class CouchbaseDataSource extends NoSQLDataSource  {
+	private $strHost;
+	private $intPort;
 	private $strUserName;
 	private $strPassword;
 	
 	private $objBucketInfo;
+	
+	/**
+	 * Sets database server host name
+	 *
+	 * @param string $strHost
+	 * @return void
+	 */
+	public function setHost($strHost) {
+		$this->strHost = $strHost;
+	}
+	
+	/**
+	 * Gets database server host name
+	 *
+	 * @return string
+	 */
+	public function getHost() {
+		return $this->strHost;
+	}
+	
+	/**
+	 * Sets database server port
+	 *
+	 * @param integer $intPort
+	 * @return void
+	 */
+	public function setPort($intPort) {
+		$this->intPort = $intPort;
+	}
+	
+	/**
+	 * Gets database server port
+	 *
+	 * @return integer
+	 */
+	public function getPort() {
+		return $this->intPort;
+	}
 	
 	/**
 	 * Set bucket information.
--- src/RedisDataSource.php
+++ src/RedisDataSource.php
@@ -2,4 +2,45 @@
 /**
  * Encapsulates a data source to use for redis connections.
  */
-class RedisDataSource extends NoSQLDataSource {}
+class RedisDataSource extends NoSQLDataSource {
+	private $strHost;
+	private $intPort;
+	
+	/**
+	 * Sets database server host name
+	 *
+	 * @param string $strHost
+	 * @return void
+	 */
+	public function setHost($strHost) {
+		$this->strHost = $strHost;
+	}
+	
+	/**
+	 * Gets database server host name
+	 *
+	 * @return string
+	 */
+	public function getHost() {
+		return $this->strHost;
+	}
+	
+	/**
+	 * Sets database server port
+	 *
+	 * @param integer $intPort
+	 * @return void
+	 */
+	public function setPort($intPort) {
+		$this->intPort = $intPort;
+	}
+	
+	/**
+	 * Gets database server port
+	 *
+	 * @return integer
+	 */
+	public function getPort() {
+		return $this->intPort;
+	}
+}
