e3cf165bb2c5afb2a0ef77076a77e358b7a78bbc
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2020-01-03 08:23:47 +0200

    bugfix

--- src/ConnectionSingleton.php
+++ src/ConnectionSingleton.php
@@ -38,10 +38,9 @@ class ConnectionSingleton
      */
     public static function getInstance(): Driver
     {
-        if (self::$instance) {
-            return self::$instance->getConnection();
+        if (!self::$instance) {
+            self::$instance = new ConnectionSingleton();
         }
-        self::$instance = new ConnectionSingleton();
         return self::$instance->getConnection();
     }
     
